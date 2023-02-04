bafc5804052021b9567c05e2804f5d469a2d0ac9
aallen5636 <aallen5636@gmail.com>
2023-02-03 18:59:49 -0600

    v3 Update

--- v2_listening_worker.py
+++ v2_listening_worker.py
@@ -1,102 +1,97 @@
 """
-Author: Ashley Allen
-Date: January 231, 2023
+    This program reads tasks from tasks.csv and sends to a queue on the RabbitMQ server.
+    Make tasks harder/longer-running by adding dots at the end of the message.
 
-    This program listens for work messages contiously. 
-    Start multiple versions to add more workers.  
-
-    Author: Denise Case
-    Date: January 15, 2023
-
+    Author: Ashley Allen
+    Date: February 3, 2023
 """
 
 import pika
 import sys
-import time
-
-# define a callback function to be called when a message is received
-def callback(ch, method, properties, body):
-    """ Define behavior on getting a message."""
-    # decode the binary message body to a string
-    print(f" [x] Received {body.decode()}")
-    # simulate work by sleeping for the number of dots in the message
-    time.sleep(body.count(b"."))
-    # when done with task, tell the user
-    print(" [x] Done.")
-    # acknowledge the message was received and processed 
-    # (now it can be deleted from the queue)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+import webbrowser
+# Import the below module whenever csv is involved 
+import csv
 
 
-# define a main function to run the program
-def main(hn: str = "localhost", qn: str = "task_queue"):
-    """ Continuously listen for task messages on a named queue."""
 
-    # when a statement can go wrong, use a try-except block
+def offer_rabbitmq_admin_site():
+    """Offer to open the RabbitMQ Admin website"""
+    ans = input("Would you like to monitor RabbitMQ queues? y or n ")
+    print()
+    if ans.lower() == "y":
+        webbrowser.open_new("http://localhost:15672/#/queues")
+        print()
+
+def send_message(host: str, queue_name: str, message):
+    """
+    Creates and sends a message to the queue each execution.
+    This process runs and finishes.
+    Parameters:
+        host (str): the host name or IP address of the RabbitMQ server
+        queue_name (str): the name of the queue
+        message (str): the message to be sent to the queue
+    """
+
     try:
-        # try this code, if it works, keep going
         # create a blocking connection to the RabbitMQ server
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hn))
-
-    # except, if there's an error, do this
-    except Exception as e:
-        print()
-        print("ERROR: connection to RabbitMQ server failed.")
-        print(f"Verify the server is running on host={hn}.")
-        print(f"The error says: {e}")
-        print()
-        sys.exit(1)
-
-    try:
+        conn = pika.BlockingConnection(pika.ConnectionParameters(host))
         # use the connection to create a communication channel
-        channel = connection.channel()
-
+        ch = conn.channel()
         # use the channel to declare a durable queue
         # a durable queue will survive a RabbitMQ server restart
         # and help ensure messages are processed in order
         # messages will not be deleted until the consumer acknowledges
-        channel.queue_declare(queue=qn, durable=True)
-
-        # The QoS level controls the # of messages
-        # that can be in-flight (unacknowledged by the consumer)
-        # at any given time.
-        # Set the prefetch count to one to limit the number of messages
-        # being consumed and processed concurrently.
-        # This helps prevent a worker from becoming overwhelmed
-        # and improve the overall system performance. 
-        # prefetch_count = Per consumer limit of unaknowledged messages      
-        channel.basic_qos(prefetch_count=1) 
-
-        # configure the channel to listen on a specific queue,  
-        # use the callback function named callback,
-        # and do not auto-acknowledge the message (let the callback handle it)
-        channel.basic_consume( queue=qn, on_message_callback=callback)
-
+        ch.queue_declare(queue=queue_name, durable=True)
+        # use the channel to publish a message to the queue
+        # every message passes through an exchange
+        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
         # print a message to the console for the user
-        print(" [*] Ready for work. To exit press CTRL+C")
-
-        # start consuming messages via the communication channel
-        channel.start_consuming()
-
-    # except, in the event of an error OR user stops the process, do this
-    except Exception as e:
-        print()
-        print("ERROR: something went wrong.")
-        print(f"The error says: {e}")
+        
+        print("Sent", message)
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error: Connection to RabbitMQ server failed: {e}")
         sys.exit(1)
-    except KeyboardInterrupt:
-        print()
-        print(" User interrupted continuous listening process.")
-        sys.exit(0)
     finally:
-        print("\nClosing connection. Goodbye.\n")
-        connection.close()
-
+        # close the connection to the server
+        conn.close()
 
 # Standard Python idiom to indicate main program entry point
 # This allows us to import this module and use its functions
 # without executing the code below.
 # If this is the program being run, then execute the code below
-if __name__ == "__main__":
-    # call the main function with the information needed
-    main("localhost", "task_queue2")
+if __name__ == "__main__":  
+    
+    # Only offer to show the prompt based on the below
+    show_offer = "False"
+
+    if show_offer == "True":
+       # ask the user if they'd like to open the RabbitMQ Admin site
+       offer_rabbitmq_admin_site()
+    else:
+        webbrowser.open_new("http://localhost:15672/#/queues")
+        print()
+
+    # Declare variables needed to read csv file as input
+    # The file name does not have to be hard coded. We can change the file name assigned to the input_file_name variable 
+    # if no arguments are provided, use the default message
+    # use the join method to convert the list of arguments into a string
+    
+
+    input_file_name = "tasks.csv"
+
+    input_file = open(input_file_name, "r")
+
+    reader = csv.reader(input_file, delimiter=",")
+
+    # header = next(reader)
+    header_list = ["message"]
+
+    # Get message from the file
+    for row in reader:
+        #Below converts list to a string. If the below is not done an error is thrown
+        message = ",".join(row)
+        # send the message to the queue
+        send_message("localhost","task_queue3",message)
+        
+
+    input_file.close()
